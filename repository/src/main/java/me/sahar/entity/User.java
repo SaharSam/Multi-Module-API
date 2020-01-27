ab5908212ab5b677375e35960fe2e7fc97be3c41
SaharSam <52576006+SaharSam@users.noreply.github.com>
2020-01-27 20:53:39 +0330

    Create Packages

---
+++ repository/src/main/java/me/sahar/entity/User.java
@@ -0,0 +1,15 @@
+package me.sahar.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class User {
+
+   private String name;
+
+}
